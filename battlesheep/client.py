075bdf174fb747560429466601595aa9d4de57f3
iwatkot <iwatkot@gmail.com>
2025-05-16 21:04:27 +0200

    Two-way mesaging.

---
+++ battlesheep/client.py
@@ -0,0 +1,10 @@
+import requests
+
+
+def make_request():
+    url = "http://192.168.100.29:8000/message"
+    payload = {"message": "Hello from the client!"}
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.post(url, json=payload, headers=headers)
+    print(response.status_code)
--- battlesheep/game.py
+++ battlesheep/game.py
@@ -1,5 +1,6 @@
 import pygame
 
+from battlesheep.client import make_request
 from battlesheep.config import ip_address
 
 
@@ -18,12 +19,21 @@ def run_pygame(message_queue):
     text = font.render(f"IP: {ip_address}", True, (255, 255, 255))
     text_rect = text.get_rect(center=(200, 150))
 
+    # Add button "Send message"
+    button_font = pygame.font.Font(None, 24)
+    button_text = button_font.render("Send message", True, (255, 255, 255))
+    button_rect = button_text.get_rect(center=(200, 250))
+
     # Main loop
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if button_rect.collidepoint(event.pos):
+                    make_request()
 
         if not message_queue.empty():
             message = message_queue.get()
@@ -34,6 +44,8 @@ def run_pygame(message_queue):
         screen.fill((0, 0, 0))
 
         # Draw the text
+        pygame.draw.rect(screen, (0, 0, 255), button_rect.inflate(20, 10))
+        screen.blit(button_text, button_rect)
         screen.blit(text, text_rect)
 
         # Update the display
